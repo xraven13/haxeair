b593499606080dde74e56599ff9328970bbb653b
Nickolay.Artamonov <Nickolay.Artamonov@39a9d917-7d50-0410-a8d6-094382878fa3>
2008-07-09 11:01:16 +0000

    - example test6 fixed

--- trunk/examples/build.bat
+++ trunk/examples/build.bat
@@ -2,4 +2,4 @@ haxe AirTest.hxml
 
 SET ADT_ROOT=..\..\trunk\build\tools\environment\air
 
-CALL %ADT_ROOT%\bin\adt.bat -package -storetype pkcs12 -keystore haXeAIR.pfx -storepass haXeAIR AirTest.air AirTest.xml -C .\ AirTest.swf
+CALL %ADT_ROOT%\bin\adt.bat -package -storetype pkcs12 -keystore haXeAIR.pfx -storepass haXeAIR AirTest.air AirTest.xml -C .\ AirTest.swf AirTest.gif
